b502a7f9169969a756962249b7ebcc58a7eff248
fufu <aichaabady0@gmail.com>
2026-05-16 16:21:12 +0200

    Initialize package.json for blabchatpro project

---
+++ package.js
@@ -0,0 +1,21 @@
+{
+  "name": "blabchatpro",
+  "version": "1.0.0",
+  "description": "dqsdqsdqsdqsdqsd",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/aichaabady0-byte/blabchatpro.git"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "type": "commonjs",
+  "bugs": {
+    "url": "https://github.com/aichaabady0-byte/blabchatpro/issues"
+  },
+  "homepage": "https://github.com/aichaabady0-byte/blabchatpro#readme"
+}
